3bc9df651c28dd764502e4c562a0663b50299a58
Vikas Rungta <runvikas@amazon.com>
2020-01-19 06:27:25 +0530

    database flow

--- server/utilities/scripMasterQuery.js
+++ server/utilities/scripMasterQuery.js
@@ -1,15 +1,18 @@
 const scripMasterQuery = {
-  postKycRequest: ({  userId,
-  PAN_number,
-  Name_On_PAN,
-  DOB,
-  Add_Proof,
-  ProofId,
-  Add_Front,
-  Add_Back,
-  PAN_Image,
-  Status}) => {
-    return `insert into [dbo].[KYCDetail](userId,
+  postKycRequest: ({
+    userId,
+    PAN_number,
+    Name_On_PAN,
+    DOB,
+    Add_Proof,
+    ProofId,
+    Status,
+    Add_Front,
+    Add_Back,
+    PAN_Image
+  }) => {
+    return `insert into [dbo].[KYCDetail](
+      userId,
       PAN_number,
       Name_On_PAN,
       DOB,
@@ -19,8 +22,8 @@ const scripMasterQuery = {
       Add_Front,
       Add_Back,
       PAN_Image
-      ) values("${userId}", ${PAN_number}, "${Name_On_PAN}", convert(date,"${DOB}"),
-      ${Add_Proof}, ${ProofId}, ${Status}, ${Add_Front}, ${Add_Back}, ${PAN_Image})`
+      ) values('${userId}', ${PAN_number}, '${Name_On_PAN}', convert(date,'${DOB}'),
+      ${Add_Proof}, '${ProofId}', ${Status}, ${Add_Front}, ${Add_Back}, ${PAN_Image})`;
   }
 };
 
